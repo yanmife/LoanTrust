80da73641666881f4bd440dce795dc4550714035
yanmife <yanmifeojo@gmail.com>
2018-08-20 06:12:40 +0100

    Update employerDetails.js

--- api/models/employerDetails.js
+++ api/models/employerDetails.js
@@ -12,7 +12,7 @@ const EmployerDetails = {
     addEmployerDetails: function(EmployerDetails, callback) {
         const dt = new Date();
 
-        return db.query("INSERT INTO employer_details(primary_employer_address, official_email_secondary_employer, resumption_branch, landmark, lga, year_of_employment, idNumber, employmentTime, netMonthlyIncome, user) VALUES(?,?,?,?,?,?,?,?,?,?)", [EmployerDetails.primary_employer_address, EmployerDetails.official_email_secondary_employer, EmployerDetails.resumption_branch, EmployerDetails.landmark, EmployerDetails.lga, EmployerDetails.year_of_employment, EmployerDetails.idNumber, EmployerDetails.employmentTime, EmployerDetails.netMonthlyIncome, EmployerDetails.user, dt], callback);
+        return db.query("INSERT INTO employer_details(primaryEmployer, primaryEmployer-Email, secondaryemployerAddress, payDate, primary_employer_address, official_email_secondary_employer, resumption_branch, landmark, lga, year_of_employment, idNumber, employmentTime, netMonthlyIncome, user) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)", [EmployerDetails.primary_employer_address, EmployerDetails.primaryEployer, EmployerDetails.primaryEmployer_email, EmployerDetails.secondaryemployerAddress, EmployerDetails.payDate, EmployerDetails.official_email_secondary_employer, EmployerDetails.resumption_branch, EmployerDetails.landmark, EmployerDetails.lga, EmployerDetails.year_of_employment, EmployerDetails.idNumber, EmployerDetails.employmentTime, EmployerDetails.netMonthlyIncome, EmployerDetails.user, dt], callback);
     },
 
     deleteEmployerDetails: function(id, callback) {
@@ -20,7 +20,7 @@ const EmployerDetails = {
     },
 
     updateEmployerDetails: function(id, EmployerDetails, callback) {
-        return db.query("UPDATE employer_details SET primary_employer_address =?, official_email_secondary_employer =?, resumption_branch =?, landmark =?, lga =?, year_of_employment =?, idNumber =?, employmentTime =?, netMonthlyIncome =? WHERE employer_details_id = ?", [EmployerDetails.primary_employer_address, EmployerDetails.official_email_secondary_employer, EmployerDetails.resumption_branch, EmployerDetails.landmark, EmployerDetails.local_government, EmployerDetails.year_of_employment, EmployerDetails.idNumber, EmployerDetails.employmentTime, EmployerDetails.netMonthlyIncome, id], callback);
+        return db.query("UPDATE employer_details SET primary_employer_address =?, official_email_secondary_employer =?, resumption_branch =?, landmark =?, lga =?, year_of_employment =?, primaryEmployer =?, primaryEmployer_email= ?, secondaryemployerAddress =?, payDate =?, idNumber =?, employmentTime =?, netMonthlyIncome =? WHERE employer_details_id = ?", [EmployerDetails.primary_employer_address, EmployerDetails.official_email_secondary_employer, EmployerDetails.primaryEmployer, EmployerDetails.primaryEmployer_email, EmployerDetails.secondaryemployerAddress, EmployerDetails.payDate, EmployerDetails.resumption_branch, EmployerDetails.landmark, EmployerDetails.local_government, EmployerDetails.year_of_employment, EmployerDetails.idNumber, EmployerDetails.employmentTime, EmployerDetails.netMonthlyIncome, id], callback);
     }
 };
 
